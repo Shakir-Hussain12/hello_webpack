255557cb3a758a6dc70501b1be089d9859dee02e
Shakir Hussain <shakir.hussain6969@gmail.com>
2023-06-06 20:34:52 +0500

    Added routes, redux store, and features to the app.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 import App from "./components/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
       <Router>
         <App />
       </Router>
+    </Provider>
   </React.StrictMode>
 );
